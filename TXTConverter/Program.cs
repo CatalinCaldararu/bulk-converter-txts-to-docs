d200f3786221aa454f1435f6fb058b8b0abf988a
myusername <catalin.caldararu99@gmail.com>
2021-10-02 19:24:59 +0300

    Initial commit

---
+++ TXTConverter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using Spire.Doc;
+
+namespace CreateWordFromTxt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Document doc;
+            string fileNameWithoutExtension;
+            string outputFilePath;
+
+            Console.WriteLine("Enter the path of your input folder:");
+            string inputFolder = Console.ReadLine();
+
+            Console.WriteLine("Enter the path of your output folder:");
+            string outputFolder = Console.ReadLine();
+            outputFolder = String.Format(outputFolder + @"\");
+
+            DirectoryInfo folder = new DirectoryInfo(inputFolder);
+            foreach (FileInfo file in folder.GetFiles())
+            {
+                doc = new Document();
+                doc.LoadText(file.FullName);
+                fileNameWithoutExtension = Path.GetFileNameWithoutExtension(file.FullName);
+                outputFilePath = String.Format(outputFolder + fileNameWithoutExtension + ".docx");
+                doc.SaveToFile(outputFilePath, FileFormat.Docx2013);
+            }
+
+        }
+    }
+}
